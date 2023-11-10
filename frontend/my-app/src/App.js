40a87d25dc52edc1211a91c61198cdec49e85d5c
MargotHuet <margothuet4@gmail.com>
2023-11-10 19:16:18 +0100

    fix

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        {/*<img src={} className="background" alt="logo" />*/}
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/my-app/src/Navbar.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import axios from 'axios';
+import { Link } from "react-router-dom";
+// @material-ui/core components
+
+export function Navbar():
+    return (
+        <Navbar>
+            <Container>
+                <Link></Link>
+            </Container>
+        </Navbar>
+    )
